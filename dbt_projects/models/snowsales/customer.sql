c5e6ff0e2bc8cd505903addee449dcc887e2e069
Pavan Narni <mail.pavan08@gmail.com>
2021-04-30 23:02:48 -0500

    added variables

--- dbt_projects/models/snowsales/customer.sql
+++ dbt_projects/models/snowsales/customer.sql
@@ -3,7 +3,9 @@
     with customer_base as (
 
      select *,
-     {{ dbt_utils.surrogate_key(['C_NAME', 'C_NATIONKEY']) }} as C_SURROGAE_KEY from {{ source('SNOWFLAKE_SAMPLE_DATA', 'customer') }}
+     {{ dbt_utils.surrogate_key(['C_NAME', 'C_NATIONKEY']) }} as C_SURROGAE_KEY ,
+     C_MKTSEGMENT ='{{ var('preferred_segment') }}' as is_preferred_segment
+     from {{ source('SNOWFLAKE_SAMPLE_DATA', 'customer') }}
 
     	),
 
